551bbd2b6eaec0a77a82004e6fa30ae910fe444d
Alberto Garcia <alb.garciam@gmail.com>
2020-01-14 18:44:13 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -14,16 +14,11 @@ let package = Package(
             name: "CardStyleTransition",
             targets: ["CardStyleTransition"]),
     ],
-    dependencies: [
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CardStyleTransition",
-        .testTarget(
-            name: "CBConnectivityTests",
-            dependencies: ["CardStyleTransition"]),
+            path: "CardTransition/Classes"
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
